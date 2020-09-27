c53670654fc95e25f80a2b756c79d9b0a9feca6a
Frederick O'Brien <frederik.obrien@gmail.com>
2020-09-27 18:31:23 +0200

    Added RSS feed, I think

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addPassthroughCopy("css");
-  };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("css");
+};
+
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+};
